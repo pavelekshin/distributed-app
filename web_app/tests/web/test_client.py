d89d71256b58d94f8c96b6b30ff86138e2148fe3
Pavel Ekshin <pavel.ekshine@gmail.com>
2024-08-20 23:19:41 +0300

    add test

--- web_app/tests/web/test_client.py
+++ web_app/tests/web/test_client.py
@@ -16,7 +16,7 @@ async def test_dummy_url(client: TestClient):
     assert resp.status == 404
 
 
-async def test_validate_code(client: TestClient):
+async def test_validate_not_found(client: TestClient):
     resp: ClientResponse = await client.post("/123/validate")
     assert resp.status == 404
     assert await resp.json() == {
@@ -24,3 +24,40 @@ async def test_validate_code(client: TestClient):
         "error_message": "Code not found",
         "detail": "Code not found",
     }
+
+
+@pytest.mark.parametrize(
+    "redirect, expected",
+    [
+        (False, 302),
+        (True, 200),
+    ],
+)
+async def test_validate_redirect(
+        client: TestClient,
+        monkeypatch: pytest.MonkeyPatch,
+        redirect,
+        expected,
+) -> None:
+    from src.routes import service
+    from src.routes import client as client_module
+
+    async def fake_request(*args, **kwargs):
+        return {
+            "id": 10756,
+            "message_id": "0",
+            "code": "Acos0csqu3E",
+            "data": '{"acc_id":"0","unsubscribe":false,"msg_id":"0"}',
+            "original_url": "https://pypi.org/",
+            "created_at": datetime.datetime.now(),
+        }
+
+    async def fake_validation(*args, **kwargs):
+        return True
+
+    monkeypatch.setattr(service, "get_row_by_code", fake_request)
+    monkeypatch.setattr(client_module, "check_resource", fake_validation)
+    monkeypatch.setattr(service, "insert_message", fake_validation)
+
+    resp: ClientResponse = await client.post("/123/validate", allow_redirects=redirect)
+    assert resp.status == expected
